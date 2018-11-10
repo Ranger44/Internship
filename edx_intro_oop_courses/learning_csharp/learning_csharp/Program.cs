913563b8c733aad6118d7279cff3cb48f954575c
hs <hstraleydeveloper@gmail.com>
2018-11-10 12:36:12 -0700

    orgnaizing folders

--- edx_intro_oop_courses/learning_csharp/learning_csharp/Program.cs
+++ edx_intro_oop_courses/learning_csharp/learning_csharp/Program.cs
@@ -13,42 +13,42 @@ namespace learning_csharp
         static void Main(string[] args)
         {
             #region mod3_labs
-            // Creates StreamReader object called "streamReaderObject" 
-            // Assigns its value to null
-            StreamReader streamReaderObject = null;
+            //// Creates StreamReader object called "streamReaderObject" 
+            //// Assigns its value to null
+            //StreamReader streamReaderObject = null;
 
-            try
-            {
-                // Assigns "streamReaderObject" to read from a text file named "file1"
-                streamReaderObject = new StreamReader("file1.txt");
+            //try
+            //{
+            //    // Assigns "streamReaderObject" to read from a text file named "file1"
+            //    streamReaderObject = new StreamReader("file1.txt");
 
-                // Reads all characters from the current position to the end of the stream
-                // Stores in String variable "contents"
-                String contents = streamReaderObject.ReadToEnd();
+            //    // Reads all characters from the current position to the end of the stream
+            //    // Stores in String variable "contents"
+            //    String contents = streamReaderObject.ReadToEnd();
 
-                // Closes StreamReader
-                streamReaderObject.Close();
+            //    // Closes StreamReader
+            //    streamReaderObject.Close();
 
-                // Writes the amount of text elements in the text file to the Console
-                Console.WriteLine("The file has {0} text elements.", new StringInfo(contents).LengthInTextElements);
-            }
+            //    // Writes the amount of text elements in the text file to the Console
+            //    Console.WriteLine("The file has {0} text elements.", new StringInfo(contents).LengthInTextElements);
+            //}
 
-            // Code to handle any errors
-            catch (FileNotFoundException)
-            {
-                // Informs user there is no file created
-                Console.WriteLine("The file cannot be found.");
-            }
-            // Invoking the Dispose method in a finally block
-            // Note that code in finally block will always execute
-            finally
-            {
-                // Checks if object is not null
-                if (streamReaderObject != null)
-                    // Calls upon the Dispose method
-                    // Releases all resources used by the TextReader object
-                    streamReaderObject.Dispose();
-            }
+            //// Code to handle any errors
+            //catch (FileNotFoundException)
+            //{
+            //    // Informs user there is no file created
+            //    Console.WriteLine("The file cannot be found.");
+            //}
+            //// Invoking the Dispose method in a finally block
+            //// Note that code in finally block will always execute
+            //finally
+            //{
+            //    // Checks if object is not null
+            //    if (streamReaderObject != null)
+            //        // Calls upon the Dispose method
+            //        // Releases all resources used by the TextReader object
+            //        streamReaderObject.Dispose();
+            //}
             #endregion
 
             #region mod2_self_assess
