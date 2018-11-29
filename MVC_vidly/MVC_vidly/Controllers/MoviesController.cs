cef287bb38223262b982018dcc5164cafa6d0716
hs <hstraleydeveloper@gmail.com>
2018-11-29 13:45:25 -0700

    mvc course routing practice and notes

--- MVC_vidly/MVC_vidly/Controllers/MoviesController.cs
+++ MVC_vidly/MVC_vidly/Controllers/MoviesController.cs
@@ -13,8 +13,30 @@ namespace MVC_vidly.Controllers
         public ActionResult Random()  //or public ViewResult Random()
         {
             var movie = new Movie(){Name = "Shrek!"};
-            //return View(movie);
-            return RedirectToAction("Index", "Home", new { page = 1, sortBy = "name" });
+            return View(movie);
+            //return RedirectToAction("Index", "Home", new { page = 1, sortBy = "name" });
+        }   
+        // GET: movies/edit/1 or movies/edit?id=1
+        public ActionResult Edit(int id)
+        {
+            return Content("ID = " + id);
         }
+        public ActionResult Index(int? pageIndex, string sortBy)  //int? make pageIndex nullable
+        {
+            if(!pageIndex.HasValue) {
+                pageIndex = 1;
+            }
+            if(String.IsNullOrWhiteSpace(sortBy))
+            {
+                sortBy = "Name";
+            }
+            return Content(String.Format("pageIndex={0}&sortBy={1}", pageIndex, sortBy));
+        }
+        [Route("movies/released/{year}/{month:regex(\\d{2}):range(1,12)}")]
+        public ActionResult ByReleaseDate(int year, int month)
+        {
+            return Content(year + "/" + month);
+        }
+
     }
 }
