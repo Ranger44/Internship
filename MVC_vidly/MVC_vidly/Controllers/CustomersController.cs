df906deaf643521211b60f4819fa154132949637
hs <hstraleydeveloper@gmail.com>
2018-12-03 20:21:32 -0700

    more mvc course practice

---
+++ MVC_vidly/MVC_vidly/Controllers/CustomersController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MVC_vidly.Models;
+using MVC_vidly.ViewModels;
+
+namespace MVC_vidly.Controllers
+{
+    public class CustomersController : Controller
+    {
+        // GET: Customers
+        public ActionResult Index()
+        {
+            var customers = new List<Customer>
+            {
+                new Customer() {Name = "John Smith"},
+                new Customer() {Name = "Mary Williams"}
+            };
+
+            return View(customers);
+            //return Content(String.Format("customers page"));
+        }
+    }
+}
---
+++ MVC_vidly/MVC_vidly/ViewModels/CustomersViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using MVC_vidly.Models;
+
+namespace MVC_vidly.ViewModels
+{
+    public class CustomersViewModel
+    {
+        public List<Customer> Customers { get; set; }
+
+    }
+}
